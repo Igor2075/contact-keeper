d71ee240803b571e1d5c3a7180f498107057f3a8
Igor <rogier.van.amersfoort.75@gmail.com>
2020-05-07 15:32:02 -0300

    End Point Routes created

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const connectDB = require("./config/db");
 
 const app = express();
+
 connectDB();
 app.get("/", (req, res) =>
 	res.json({ msg: "Welcome to the Contact Keeper API..." })
 );
+
+app.use(express.json({ extended: false }));
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
